5daca6ee2fb1262f4de318f89d809ed5fe583016
Sgt  EDER <ederelfs@SGT-EDER.ccasj.intraer>
2024-02-20 09:45:35 -0300

    Menu principal e Fundamentos

---
+++ src/app/menu/menuPrincipal.ts
@@ -0,0 +1,22 @@
+import TerminalUtil from "@/util/terminalUtil";
+import { terminal } from "terminal-kit";
+import menuFundamentos from "./menuFundamentos";
+
+export default async function menuPrincipal() {
+  TerminalUtil.titulo("Menu Principal");
+
+  const resposta = await terminal.singleColumnMenu([
+    '1. Fundamentos',
+    'Sair'
+  ]).promise;
+
+  switch (resposta.selectedIndex) {
+    case 0:
+      await menuFundamentos();
+      break;
+    case 1:
+      process.exit(0);
+  }
+
+  menuPrincipal();
+}
