1248f8c552366a7d80ad70536afa59729469b990
Sgt  EDER <ederelfs@SGT-EDER.ccasj.intraer>
2024-02-26 14:21:32 -0300

    Removendo comentários não utilizados

--- src/core/usuario/service/ProvedorCriptografia.ts
+++ src/core/usuario/service/ProvedorCriptografia.ts
@@ -1,10 +1,3 @@
-
-// TODO - Passar para o caderno depois:
-// Interfaces são Portas e as portas fazem parte do CORE da aplicação
-// Essas portas definem as demandas de negócio da aplicação
-
-// No Core, consideramos a camada de entidade e a camadas de Casos de Uso
-
 export default interface ProvedorCriptografia {
   criptografar(senha: string): string;
 }
